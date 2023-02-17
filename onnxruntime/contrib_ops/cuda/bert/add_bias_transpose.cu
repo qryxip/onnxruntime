b7b36cea52c85e5fd6bf5e47464d94e3fe22f8b8
Ubuntu <wy@v100-2.0cdb2e52twzevn1i4fi45bylyg.jx.internal.cloudapp.net>
2023-02-17 22:57:08 +0000

    d

--- onnxruntime/contrib_ops/cuda/bert/add_bias_transpose.cu
+++ onnxruntime/contrib_ops/cuda/bert/add_bias_transpose.cu
@@ -1,37 +1,35 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-#pragma once
-
 #include "core/providers/cuda/cuda_common.h"
 #include "core/providers/cuda/cu_inc/common.cuh"
 #include "contrib_ops/cuda/bert/add_bias_transpose.h"
 #include "contrib_ops/cuda/bert/rotary_embedding_util.h"
 
-namespace onnxruntime {
-namespace cuda {
+// namespace onnxruntime {
+// namespace cuda {
 
-struct __align__(8) Half4 {
-  half2 x;
-  half2 y;
-};
+// struct __align__(8) Half4 {
+//   half2 x;
+//   half2 y;
+// };
 
-__device__ __forceinline__ Half4 operator+(const Half4& a, const Half4& b) {
-  Half4 r;
-  r.x = a.x + b.x;
-  r.y = a.y + b.y;
-  return r;
-}
+// __device__ __forceinline__ Half4 operator+(const Half4& a, const Half4& b) {
+//   Half4 r;
+//   r.x = a.x + b.x;
+//   r.y = a.y + b.y;
+//   return r;
+// }
 
-__device__ __forceinline__ float2 operator+(const float2& a, const float2& b) {
-  return make_float2(a.x + b.x, a.y + b.y);
-}
+// __device__ __forceinline__ float2 operator+(const float2& a, const float2& b) {
+//   return make_float2(a.x + b.x, a.y + b.y);
+// }
 
-__device__ __forceinline__ float4 operator+(const float4& a, const float4& b) {
-  return make_float4(a.x + b.x, a.y + b.y, a.z + b.z, a.w + b.w);
-}
+// __device__ __forceinline__ float4 operator+(const float4& a, const float4& b) {
+//   return make_float4(a.x + b.x, a.y + b.y, a.z + b.z, a.w + b.w);
+// }
 
-}  // namespace cuda
-}  // namespace onnxruntime
+// }  // namespace cuda
+// }  // namespace onnxruntime
 
 using namespace onnxruntime::cuda;
 
